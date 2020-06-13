b3c868124de43a6f970b0fb628a80e8031a03dc8
Mayank Verma <mayank.verma@dish.com>
2020-06-14 00:54:04 +0530

    Added one more step to test.

--- src/sling/pipeline/Utilities.groovy
+++ src/sling/pipeline/Utilities.groovy
@@ -4,5 +4,8 @@ class Utilities implements Serializable {
   Utilities(steps) {this.steps = steps}
   def sampleUtility() {
     steps.sh "echo 'Hello world'"
+
+    //Running Pwd command.
+    steps.sh "echo 'pwd'"
   }
 }
