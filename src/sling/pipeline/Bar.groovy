0f9d22407573132db3405e153f9111ccc8de5fb3
Mayank Verma <mayank.verma@dish.com>
2020-06-14 00:08:44 +0530

    Added Initial set of Jenkins shared libs file

---
+++ src/sling/pipeline/Bar.groovy
@@ -0,0 +1,13 @@
+// src/org/foo/Point.groovy
+package sling.pipeline
+
+// point in 3D space
+class Point {
+  float x,y,z
+}
+
+def sampleMethod() {
+  echo "This is first method from the shared library"
+}
+
+return this
