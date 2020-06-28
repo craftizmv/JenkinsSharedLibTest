95b371d1999b360c4b5640947c0831b4adfb08c2
Mayank Verma <mayank.verma@dish.com>
2020-06-28 19:00:25 +0530

    Some change.

--- src/sling/pipeline/Utilities.groovy
+++ src/sling/pipeline/Utilities.groovy
@@ -11,7 +11,7 @@ class Utilities implements Serializable {
 
 
   def sampleNewUtility() {
-    println(" App version is : ${steps.env.APP_VERSION}")
+    echo " App version is : ${steps.env.APP_VERSION}"
   }
 
 }
