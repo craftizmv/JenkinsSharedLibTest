57a73140c688bd354092bb9f1bb771e0c4994c99
Mayank Verma <mayank.verma@dish.com>
2020-06-28 19:18:47 +0530

    chage

--- src/sling/pipeline/Utilities.groovy
+++ src/sling/pipeline/Utilities.groovy
@@ -13,7 +13,7 @@ class Utilities implements Serializable {
 
 
   def sampleNewUtility() {
-    steps.sh 'echo "App version is : ${APP_VERSION}"'
+    steps.sh 'echo "App version is : ${env.APP_VERSION}"'
   }
 
 }
