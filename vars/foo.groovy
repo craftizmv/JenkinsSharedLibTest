87ce5d8b16587fa6d85e909f7e05d359f47967e3
Mayank Verma <mayank.verma@dish.com>
2020-06-28 19:26:27 +0530

    Some change.

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,6 +1,11 @@
 
 
 def info() {
+
+    println("This is inside foo info")
+
+    echo "This is inside foo info"
+
     steps.sh 'echo "App version is : ${APP_VERSION}"'
 
     steps.sh 'echo "temp param is : ${Temp}"'
